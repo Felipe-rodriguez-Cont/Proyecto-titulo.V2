4c1299f6d313a0b7cb9bc9ae088ee8f27c2577dc
Felipe-rodriguez-Cont <felipehugo.1234@gmail.com>
2023-09-23 21:21:59 -0300

    cambio
    cambio de planes y se da inicio al video antiguo pero con las mejoras de inmediato si resultan.

--- erpPyme/erpPyme/urls.py
+++ erpPyme/erpPyme/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path("Direccion/ archivo al cual queremos acceder")
-    path("login/",include("login.urls")),
+    path('',views.index),
 ]
--- erpPyme/login/models.py
+++ erpPyme/login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class usuario(models.Model):
+    #creamos un id con numeros consecutivos y con llave primaria.
+    id = models.AutoField(primary_key=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
